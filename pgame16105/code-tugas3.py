02f891e0f8011498896ca6ef79f8795febd75071
MilaKarmilaSolisa <57754493+MilaKarmilaSolisa@users.noreply.github.com>
2019-11-14 21:03:49 +0900

    tugas3

---
+++ pgame16105/code-tugas3.py
@@ -0,0 +1,11 @@
+# Ini Tugas 3. Praktik Git untuk MK Pemrograman Game
+
+# Latihan Buat Variabel : 
+nama = "Nama 		: mila karmila solisa" 
+print (nama)
+npm = "NPM 		: 121055520116105" 
+print (npm)
+semester = "Semester 	: VII (Tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)

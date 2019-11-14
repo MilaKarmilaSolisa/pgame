31b9a22334a01c62c659f0e1f747855d69fe18d3
MilaKarmilaSolisa <57754493+MilaKarmilaSolisa@users.noreply.github.com>
2019-11-14 20:57:24 +0900

    Delete tugas2.py

--- pgame16105/tugas2.py
+++
@@ -1,7 +0,0 @@
-# Ini Tugas 2. Praktik Git untuk MK Pemrograman Game
-
-print ("Hello, my friends") #Halo, teman-teman
-print ("my name is mila karmila solisa") #nama saya mila karmila solisa
-#baris ini tidak akan dieksekusi
-print ("Hallo Teknik Info UMMU") #baris ini dieksekusi
-#Baris ini juga merupakan Baris Komentar 

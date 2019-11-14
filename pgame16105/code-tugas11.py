9dc42ab5f8676439b8483c220b8aed6127a5fb45
MilaKarmilaSolisa <57754493+MilaKarmilaSolisa@users.noreply.github.com>
2019-11-14 22:57:21 +0900

    Pgame_Operator_prak_3

---
+++ pgame16105/code-tugas11.py
@@ -0,0 +1,13 @@
+#Operator Identitas
+
+a,b = 44,55
+a & b # Melakukan operasi and pada biner a dan b
+a | b # Melakukan operasi or pada biner a dan b
+a,b = 44,55
+a ^ b
+a,b = 44,55
+~a
+a,b = 44,55
+a << 2
+a,b = 44,55
+a >> 2
